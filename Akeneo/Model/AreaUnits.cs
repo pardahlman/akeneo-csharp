1f4142003521c8be0ca372f2de65807ccee431a5
pardahlman <par.dahlman@fellowdeveloper.se>
2017-04-23 19:05:09 +0200

    Add area unit
    More needs to be done here in order to fix #11

---
+++ Akeneo/Model/AreaUnits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akeneo.Model
+{
+	public class MetricFamilies
+	{
+		public const string Area = "Area";
+	}
+
+	public class AreaUnits
+	{
+		public const string SquareMillimeter = "SQUARE_MILLIMETER";
+	}
+}
