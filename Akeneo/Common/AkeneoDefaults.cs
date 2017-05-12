20eedfc6c0245311c7fa4ceb6441f0ed24ba11ba
pardahlman <par.dahlman@fellowdeveloper.se>
2017-05-12 22:10:28 +0200

    Add known default relations

--- Akeneo/Common/AkeneoDefaults.cs
+++ Akeneo/Common/AkeneoDefaults.cs
@@ -6,5 +6,9 @@
 		public const string DefaultChannel = "ecommerce";
 		public const string Category = "master";
 		public const string IdentifierAttribute = "sku";
+		public const string PackAssociation = "PACK";
+		public const string SubstitutionAssociation = "SUBSTITUTION";
+		public const string UpsellAssociation = "UPSELL";
+		public const string CrossSellAssociation = "X_SELL";
 	}
 }
