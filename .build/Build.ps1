bcccf06c2519a311fe087bc751fc9e06696941ed
pardahlman <par.dahlman@fellowdeveloper.se>
2017-06-07 19:10:18 +0200

    Updating build deploy

--- .build/Build.ps1
+++
@@ -1,9 +0,0 @@
-If (Test-Path "/artifacts"){
-    Write-Output "Remvoing old artifacts"
-	Remove-Item "/artifacts"
-} else {
-    Write-Output "The artifact folder does not exist. Continuing"
-}
-
-dotnet restore
-dotnet pack ./Akeneo/Akeneo.csproj -c Release -o ../artifacts
--- .build/Test.ps1
+++
@@ -1,1 +0,0 @@
-Write-Output "No tests :("
